c1c7954c52348e2a6bd4c7a967d971efe3e8a7b6
Ankit Bhatt <ankitbhatt10050.ab@gmail.com>
2025-03-26 10:48:54 +0530

    feat: signup stablize

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,10 @@
 import { auth } from "./server/auth";
 
+/**
+ * NOTE: https://github.com/nextauthjs/next-auth/issues/12731
+ * if we upgrade prima adapter it will break with next-auth
+ */
+
 export default auth((req) => {
   const isAuthenticated = !!req.auth;
 
