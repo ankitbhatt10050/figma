20816978d578da53dabed7392b9c9c08875c5f5f
Ankit Bhatt <ankitbhatt10050.ab@gmail.com>
2025-04-21 13:24:33 +0530

    feat: sidebar + useravatar added

--- src/utils.ts
+++ src/utils.ts
@@ -165,3 +165,9 @@ export function findIntersectionLayersWithRectangle(
 
   return ids;
 }
+
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length]!;
+}
